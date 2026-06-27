801d1f8d4aaa7d56fa433222565de6990854e12a
Justin-1984 <79670693+Justin-1984@users.noreply.github.com>
2026-06-28 00:24:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='benefit-manager-v6.6.2-beta';
+const CACHE='benefit-manager-v6.6.3-beta';
 const ASSETS=['./','./index.html','./manifest.json','./version.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
